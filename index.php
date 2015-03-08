c6e4d763ff81b53da19f8d051e5c3d10be5c81d9
starit <janyucheng@gmail.com>
2015-03-09 06:35:05 +0800

    bug fix

--- index.php
+++ index.php
@@ -103,11 +103,11 @@ class wechatCallbackapiTest
     /*天气查询API */
     private function weather($cityName){
        include("./weather_cityId.php");
-       $c_name=$weather_cityId[$cityName];
+       @$c_name=$weather_cityId[$cityName];
        if(!empty($c_name)){
            $json=file_get_contents("http://m.weather.com.cn/data/".$c_name.".html");
-           $data=json_decode($json);
-           if( empty($data->weatherinfo ))
+           $data=json_decode($json); //返回stdClass
+           if( !empty($data->weatherinfo ))
            {
                $contentStr = "【".$data->weatherinfo->city."天气预报】\n".$data->weatherinfo->date_y." ".$data->weatherinfo->fchh."时发布"."\n\n实时天气\n".$data->weatherinfo->weather1." ".$data->weatherinfo->temp1." ".$data->weatherinfo->wind1."\n\n温馨提示：".$data->weatherinfo->index_d."\n\n明天\n".$data->weatherinfo->weather2." ".$data->weatherinfo->temp2." ".$data->weatherinfo->wind2."\n\n后天\n".$data->weatherinfo->weather3." ".$data->weatherinfo->temp3." ".$data->weatherinfo->wind3; 
            }
@@ -117,7 +117,7 @@ class wechatCallbackapiTest
            }
 
        } else {
-           $contentStr="";
+           $contentStr="没有该城市".$cityName;
        }
        return $contentStr;
     }
