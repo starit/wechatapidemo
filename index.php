dc0fb956b9fdc8560cd891381e539aa027e363d0
starit <janyucheng@gmail.com>
2015-03-09 06:11:14 +0800

    bug fix

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ class wechatCallbackapiTest
        if(!empty($c_name)){
            $json=file_get_contents("http://m.weather.com.cn/data/".$c_name.".html");
            $data=json_decode($json);
-           if( empty($data['weatherinfo'] ))
+           if( empty($data->weatherinfo ))
            {
                $contentStr = "【".$data->weatherinfo->city."天气预报】\n".$data->weatherinfo->date_y." ".$data->weatherinfo->fchh."时发布"."\n\n实时天气\n".$data->weatherinfo->weather1." ".$data->weatherinfo->temp1." ".$data->weatherinfo->wind1."\n\n温馨提示：".$data->weatherinfo->index_d."\n\n明天\n".$data->weatherinfo->weather2." ".$data->weatherinfo->temp2." ".$data->weatherinfo->wind2."\n\n后天\n".$data->weatherinfo->weather3." ".$data->weatherinfo->temp3." ".$data->weatherinfo->wind3; 
            }
